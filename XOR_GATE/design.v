3cb6b3a4612f1e195da23f8360cfa2c8ea611e2a
KHALLIL REHMAN <81623534+Khallil973@users.noreply.github.com>
2024-04-25 00:22:54 +0500

    Add files via upload

---
+++ XOR_GATE/design.v
@@ -0,0 +1,8 @@
+module gate(A,B,Y);
+    input A,B;
+    output Y;
+
+
+    xor (Y,A,B);
+
+endmodule
---
+++ XOR_GATE/testbench.v
@@ -0,0 +1,39 @@
+module tb();
+
+    reg A,B;
+    wire Y;
+
+    gate dut(
+
+                .A(A),
+                .B(B),
+                .Y(Y)
+    );
+
+    initial begin
+        A <= 1'b0;
+        B <= 1'b0;
+        #100;
+
+        A <= 1'b0;
+        B <= 1'b1;
+        #100;
+
+        A <= 1'b1;
+        B <= 1'b0;
+        #100;
+
+        A <= 1'b1;
+        B <= 1'b1;
+        #100;
+      
+    end
+
+    initial begin
+        $dumpfile("dump.vsd");
+        $dumpvars(0);
+
+
+    end
+
+endmodule    
