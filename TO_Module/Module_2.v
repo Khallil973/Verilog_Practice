1da983cdbaa69e8a6a4ed8698f3e21ae2d891083
KHALLIL REHMAN <81623534+Khallil973@users.noreply.github.com>
2024-05-11 13:20:43 +0500

    Delete TO_Module directory

--- TO_Module/Module_2.v
+++
@@ -1,8 +0,0 @@
-module Module_2(Input_1_Module_2,Input_2_Module_2,Z);
-
-input Input_1_Module_2,Input_2_Module_2;
-output Z;
-
-assign Z =  (~(Input_1_Module_2 | Input_2_Module_2) & (Input_1_Module_2 & Input_2_Module_2));
-
-endmodule
